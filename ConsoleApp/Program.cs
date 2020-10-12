5134717bb3e451062f55dd91db5c81881da67bd4
Vadim Shmelev <vshmelev@intermedia.net>
2020-10-12 18:48:15 +0300

    Add solution

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var argsString = string.Join(";", args);
+            Console.WriteLine($"App was launched with args: {argsString}");
+            Console.WriteLine("Press a key to continue: ");
+            Console.ReadLine();
+        }
+    }
+}
